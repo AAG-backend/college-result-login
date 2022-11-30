d96caf36e50b22d8af1675a180c88467ebf1f450
Getu Taddesse <Getu_Taddesse@mckinsey.com>
2022-11-30 14:42:31 -0500

    Add footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App";
+
+const Footer = () => {
+  return (
+    <div className="footers">
+      <p>College Result Login App</p>
+      <p>2022 & Copy Right Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
